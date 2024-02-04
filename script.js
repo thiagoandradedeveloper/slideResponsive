1492315b6475215980254e71e3493771d4450149
thiagoandradedeveloper <thiagoha3@gmail.com>
2024-02-03 23:58:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    let next   = document.querySelector('.next');
+    let prev   = document.querySelector('.prev');
+    let slider = document.querySelector('.slider'); 
+
+    next.addEventListener('click',function(){
+        let slides = document.querySelectorAll('.slides');
+        slider.appendChild(slides[0]);
+    })
+    prev.addEventListener('click',function(){
+        let slides = document.querySelectorAll('.slides');
+        slider.prepend(slides[slides.length-1]);
+    })
+}
